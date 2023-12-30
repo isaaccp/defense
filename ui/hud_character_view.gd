b9296140cec9f295b2a379337dd2d0fc4f0d78f3
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-12-30 13:17:50 -0500

    Make the ready signal work on multiplayer.

--- ui/hud_character_view.gd
+++ ui/hud_character_view.gd
@@ -22,8 +22,11 @@ func is_local() -> bool:
 	
 func show_config(show: bool) -> void:
 	if show:
+		%ConfigContainer.show()
 		if is_local():
-			%ConfigContainer.show()
+			%ConfigureBehaviorButton.show()
+		else:
+			%ReadyButton.disabled = true
 	else:
 		%ConfigContainer.hide()
 
@@ -31,4 +34,13 @@ func _on_configure_behavior_button_pressed():
 	configure_behavior_selected.emit()
 
 func _on_ready_button_toggled(toggled_on: bool):
+	_ready_button_toggled(toggled_on)
+	_on_peer_ready_button_toggled.rpc(toggled_on)
+	
+@rpc("any_peer")
+func _on_peer_ready_button_toggled(toggled_on: bool):
+	_ready_button_toggled(toggled_on)
+	%ReadyButton.set_pressed_no_signal(toggled_on)
+		
+func _ready_button_toggled(toggled_on: bool):
 	readiness_updated.emit(toggled_on)
