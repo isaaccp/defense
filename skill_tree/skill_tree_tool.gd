43abba9329d05c219b1fea70467b3b8a99e411e4
Isaac Clerencia <isaac.clerencia@gmail.com>
2024-01-01 21:17:22 -0500

    More stats in skill tree tool.

--- skill_tree/skill_tree_tool.gd
+++ skill_tree/skill_tree_tool.gd
@@ -26,6 +26,7 @@ func load_skill_dir(dir_path: String, tree_type: SkillTree.TreeType) -> SkillTre
 	return skill_tree
 
 func create_skill_trees():
+	var num_skills = 0
 	var skill_tree_collection = SkillTreeCollection.new()
 	for tree_type in tree_types:
 		var tree_name = SkillTree.tree_type_filesystem_string(tree_type)
@@ -42,7 +43,8 @@ func create_skill_trees():
 		ResourceSaver.save(skill_tree, skill_tree_path)
 		var loaded_skill_tree = load(skill_tree_path) as SkillTree
 		skill_tree_collection.add(loaded_skill_tree)
-	print("Writing skill tree collection with %d trees" % skill_tree_collection.size())
+		num_skills += skill_tree.size()
+	print("Writing skill tree collection with %d trees, %d skills" % [skill_tree_collection.size(), num_skills])
 	var skill_tree_collection_path = trees_base_path + "/skill_tree_collection.tres"
 	ResourceSaver.save(skill_tree_collection, skill_tree_collection_path)
 	print(skill_tree_collection_path)
