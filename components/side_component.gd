a6eb3f2c0679eeac02d3882334bb38de94d11dd7
Isaac Clerencia <isaac.clerencia@gmail.com>
2024-01-05 20:44:50 -0500

    Make tutorial give you "Self" at the same time as Heal.

--- components/side_component.gd
+++ components/side_component.gd
@@ -7,15 +7,18 @@ class_name SideComponent
 
 @export_group("Debug")
 var ally_groups: Array[String]
+var ally_with_tower_groups: Array[String]
 var enemy_groups: Array[String]
 
 func _ready():
 	assert(side != Groups.GroupType.UNSPECIFIED)
 	if side == Groups.GroupType.CHARACTERS:
-		ally_groups = [Groups.CHARACTERS, Groups.TOWERS]
+		ally_groups = [Groups.CHARACTERS]
+		ally_with_tower_groups = [Groups.CHARACTERS, Groups.TOWERS]
 		enemy_groups = [Groups.ENEMIES]
 	else:
 		ally_groups = [Groups.ENEMIES]
+		ally_with_tower_groups = [Groups.ENEMIES]
 		enemy_groups = [Groups.CHARACTERS, Groups.TOWERS]
 
 func is_enemy_node(node: Node2D) -> bool:
@@ -39,6 +42,12 @@ func is_enemy(other: SideComponent) -> bool:
 func allies() -> Array:
 	var nodes = []
 	for group in ally_groups:
+		nodes.append_array(get_tree().get_nodes_in_group(group))
+	return nodes
+
+func allies_with_towers() -> Array:
+	var nodes = []
+	for group in ally_with_tower_groups:
 		nodes.append_array(get_tree().get_nodes_in_group(group))
 	return nodes
 
