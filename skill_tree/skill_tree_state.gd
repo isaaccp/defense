468e7d7093f2cef2e4777360714cbbde1e93645a
Isaac Clerencia <isaac.clerencia@gmail.com>
2024-01-08 19:18:50 -0500

    Migrate skill tree to keep a single list of skills.
    Interface remains the same but less code duplication and now lookups
    are through a dictionary.

--- skill_tree/skill_tree_state.gd
+++ skill_tree/skill_tree_state.gd
@@ -4,61 +4,80 @@ class_name SkillTreeState
 
 const always = preload("res://skill_tree/conditions/always.tres")
 
-# Refactor this so we don't have to repeat everything 4 times?
-# OTOH I don't think we are adding more skill types?
-@export var acquired_actions: Array[StringName]:
-	get:
-		if full_acquired:
-			return SkillManager.all_actions()
-		return acquired_actions
+@export var acquired_skills: Array[StringName] = [always.skill_name]
+@export var unlocked_skills: Array[StringName] = [always.skill_name]
 
-@export var acquired_target_selections: Array[StringName]:
-	get:
-		if full_acquired:
-			return SkillManager.all_target_selections()
-		return acquired_target_selections
-
-@export var acquired_target_sorts: Array[StringName]:
-	get:
-		if full_acquired:
-			return SkillManager.all_target_sorts()
-		return acquired_target_sorts
-
-@export var acquired_conditions: Array[StringName] = [always.skill_name]:
-	get:
-		if full_acquired:
-			return SkillManager.all_conditions()
-		return acquired_conditions
-
-@export var unlocked_actions: Array[StringName]:
-	get:
-		if full_unlocked:
-			return SkillManager.all_actions()
-		return unlocked_actions
-
-@export var unlocked_target_selections: Array[StringName]:
-	get:
-		if full_unlocked:
-			return SkillManager.all_target_selections()
-		return unlocked_target_selections
-
-@export var unlocked_target_sorts: Array[StringName]:
-	get:
-		if full_unlocked:
-			return SkillManager.all_target_sorts()
-		return unlocked_target_sorts
-
-@export var unlocked_conditions: Array[StringName] = [always.skill_name]:
-	get:
-		if full_unlocked:
-			return SkillManager.all_conditions()
-		return unlocked_conditions
-
-# If set, all actions/targets are available.
+# If set, all skills are acquired/unlocked..
 @export var full_acquired = false
 @export var full_unlocked = false
 
-# TODO: Make dictionaries so it's faster to check unlocked, etc.
+var acquired_by_name: Dictionary
+var unlocked_by_name: Dictionary
+
+var acquired_actions: Array[StringName]:
+	get:
+		if full_acquired:
+			return SkillManager.all_actions()
+		return acquired_by_name[Skill.SkillType.ACTION].keys()
+
+var acquired_target_selections: Array[StringName]:
+	get:
+		if full_acquired:
+			return SkillManager.all_target_selections()
+		return acquired_by_name[Skill.SkillType.TARGET].keys()
+
+var acquired_target_sorts: Array[StringName]:
+	get:
+		if full_acquired:
+			return SkillManager.all_target_sorts()
+		return acquired_by_name[Skill.SkillType.TARGET_SORT].keys()
+
+var acquired_conditions: Array[StringName]:
+	get:
+		if full_acquired:
+			return SkillManager.all_conditions()
+		return acquired_by_name[Skill.SkillType.CONDITION].keys()
+
+var unlocked_actions: Array[StringName]:
+	get:
+		if full_unlocked:
+			return SkillManager.all_actions()
+		return unlocked_by_name[Skill.SkillType.ACTION].keys()
+
+var unlocked_target_selections: Array[StringName]:
+	get:
+		if full_unlocked:
+			return SkillManager.all_target_selections()
+		return unlocked_by_name[Skill.SkillType.TARGET].keys()
+
+var unlocked_target_sorts: Array[StringName]:
+	get:
+		if full_unlocked:
+			return SkillManager.all_target_sorts()
+		return unlocked_by_name[Skill.SkillType.TARGET_SORT].keys()
+
+var unlocked_conditions: Array[StringName]:
+	get:
+		if full_unlocked:
+			return SkillManager.all_conditions()
+		return unlocked_by_name[Skill.SkillType.CONDITION].keys()
+
+func _init():
+	for skill_type in Skill.SkillType.values():
+		if skill_type == Skill.SkillType.UNSPECIFIED:
+			continue
+		acquired_by_name[skill_type] = {}
+		unlocked_by_name[skill_type] = {}
+	_process.call_deferred()
+
+func _process():
+	_process_skills(acquired_skills, acquired_by_name)
+	_process_skills(unlocked_skills, unlocked_by_name)
+
+func _process_skills(skills: Array[StringName], by_name: Dictionary):
+	for skill in skills:
+		by_name[skill] = true
+
 func unlocked(skill: Skill) -> bool:
 	return _skill_in(skill, StateType.UNLOCKED)
 
@@ -95,33 +114,15 @@ func _skill_in(skill: Skill, state_type: StateType):
 	assert(skill.skill_type != Skill.SkillType.UNSPECIFIED)
 	if _full(state_type):
 		return true
-	match skill.skill_type:
-		Skill.SkillType.ACTION:
-			return skill.skill_name in _actions(state_type)
-		Skill.SkillType.TARGET:
-			return skill.skill_name in _target_selections(state_type)
-		Skill.SkillType.CONDITION:
-			return skill.skill_name in _conditions(state_type)
-		Skill.SkillType.TARGET_SORT:
-			return skill.skill_name in _target_sorts(state_type)
-	return false
+	var state_skills = _skills_dict_by_state_type(state_type)
+	return skill.skill_name in state_skills[skill.skill_type]
 
 func _add_skill_to(skill: Skill, state_type: StateType):
 	assert(skill.skill_type != Skill.SkillType.UNSPECIFIED)
-	match skill.skill_type:
-		Skill.SkillType.ACTION:
-			_actions(state_type).append(skill.skill_name)
-		Skill.SkillType.TARGET:
-			_target_selections(state_type).append(skill.skill_name)
-		Skill.SkillType.CONDITION:
-			_conditions(state_type).append(skill.skill_name)
-		Skill.SkillType.TARGET_SORT:
-			_target_sorts(state_type).append(skill.skill_name)
-
-static func make_full() -> SkillTreeState:
-	var skill_tree_state = SkillTreeState.new()
-	skill_tree_state.full = true
-	return skill_tree_state
+	var skills = _skills_by_state_type(state_type)
+	var state_skills = _skills_dict_by_state_type(state_type)
+	skills.append(skill.skill_name)
+	state_skills[skill.skill_type][skill.skill_name] = true
 
 # For handling unlocked/acquired more easily.
 enum StateType {
@@ -129,29 +130,17 @@ enum StateType {
 	ACQUIRED,
 }
 
-func _actions(state_type: StateType) -> Array[StringName]:
+func _skills_by_state_type(state_type: StateType) -> Array[StringName]:
 	if state_type == StateType.ACQUIRED:
-		return acquired_actions
+		return acquired_skills
 	else:
-		return unlocked_actions
+		return unlocked_skills
 
-func _target_selections(state_type: StateType) -> Array[StringName]:
+func _skills_dict_by_state_type(state_type: StateType) -> Dictionary:
 	if state_type == StateType.ACQUIRED:
-		return acquired_target_selections
+		return acquired_by_name
 	else:
-		return unlocked_target_selections
-
-func _target_sorts(state_type: StateType) -> Array[StringName]:
-	if state_type == StateType.ACQUIRED:
-		return acquired_target_sorts
-	else:
-		return unlocked_target_sorts
-
-func _conditions(state_type: StateType) -> Array[StringName]:
-	if state_type == StateType.ACQUIRED:
-		return acquired_conditions
-	else:
-		return unlocked_conditions
+		return unlocked_by_name
 
 func _full(state_type: StateType) -> bool:
 	if state_type == StateType.ACQUIRED:
@@ -162,11 +151,5 @@ func _full(state_type: StateType) -> bool:
 # Used for tutorial levels, etc that need to add to a tree.
 # TODO: Don't add duplicates.
 func add(other: SkillTreeState):
-	unlocked_actions += other.unlocked_actions
-	unlocked_target_selections += other.unlocked_target_selections
-	unlocked_target_sorts += other.unlocked_target_sorts
-	unlocked_conditions += other.unlocked_target_selections
-	acquired_actions += other.acquired_actions
-	acquired_target_selections += other.acquired_target_selections
-	acquired_target_sorts += other.acquired_target_sorts
-	acquired_conditions += other.acquired_target_selections
+	unlocked_skills += other.unlocked_skills
+	acquired_skills += other.acquired_skills
