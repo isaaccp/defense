26e7081667e4a36d1bcd951c813aa40b89ce4461
Isaac Clerencia <isaac.clerencia@gmail.com>
2024-01-10 20:54:06 -0500

    Remove no longer needed Rule.make().

--- behavior/rule.gd
+++ behavior/rule.gd
@@ -2,8 +2,6 @@ extends Resource
 
 class_name Rule
 
-const always = preload("res://skill_tree/conditions/always.tres")
-
 @export var target_selection: TargetSelectionDef
 @export var action: ActionDef
 @export var condition: ConditionDef
@@ -21,11 +19,3 @@ func _to_string() -> String:
 		target_selection.name(),
 		condition.name(),
 	]
-
-static func make(target_selection: TargetSelectionDef, action: ActionDef, condition: ConditionDef = always) -> Rule:
-	var rule = Rule.new()
-	rule.target_selection = target_selection
-	rule.action = action
-	assert(not condition.abstract)
-	rule.condition = condition
-	return rule
