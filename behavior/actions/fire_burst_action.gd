e3fd72480a83e4bd609c1b810c910faa557028af
Isaac Clerencia <isaac.clerencia@gmail.com>
2024-01-18 21:34:15 -0500

    Add Fire Burst action.
    Work for #26.

---
+++ behavior/actions/fire_burst_action.gd
@@ -0,0 +1,24 @@
+extends Action
+
+const fire_burst_scene = preload("res://behavior/actions/scenes/fire_burst.tscn")
+
+var fire_burst: ActionScene
+
+func _init():
+	max_distance = 80
+	cooldown = 5.0
+
+func post_initialize():
+	_fire_burst.call_deferred()
+
+func _fire_burst():
+	var dir = (target.position(Target.PositionType.HURTBOX) - body.position).normalized()
+	fire_burst = fire_burst_scene.instantiate() as ActionScene
+	_initialize_action_scene(fire_burst)
+	fire_burst.look_at(fire_burst.position + dir)
+	fire_burst.position += dir * 15
+	action_sprites.add_child(fire_burst)
+	Global.get_tree().create_timer(1.0, false).timeout.connect(action_finished)
+
+func description():
+	return "Creates a burst of fire in front of the caster."
