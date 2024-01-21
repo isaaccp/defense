9098e7516bf0d1cb3d17a64f59abdf393f4a3227
Isaac Clerencia <isaac.clerencia@gmail.com>
2024-01-21 08:18:01 -0500

    Move restoring behavior to SkillManager. It removes a lot of cycles.
    2k cycles down:
    
    $ ./dot_find_cycles.py Digraph.gv | wc -l
    1292
    
    Work for #47.

--- behavior/stored_behavior.gd
+++ behavior/stored_behavior.gd
@@ -8,16 +8,6 @@ class_name StoredBehavior
 ## Rules.
 @export var stored_rules: Array[RuleDef]
 
-# When behavior is set through editor/etc, we don't write (well, we do for now,
-# but we want to change it) the full skill definition, but just RuleSkillDefs.
-# From there we can create the full instance using the SkillManager.
-func restore() -> Behavior:
-	var behavior = Behavior.new()
-	for stored_rule in stored_rules:
-		var rule = SkillManager.restore_rule(stored_rule)
-		behavior.rules.append(rule)
-	return behavior
-
 func serialize() -> PackedByteArray:
 	var data = []
 	#for stored_rule in stored_rules:
@@ -31,13 +21,7 @@ func serialize() -> PackedByteArray:
 static func deserialize(serialized_behavior: PackedByteArray) -> StoredBehavior:
 	var behavior = StoredBehavior.new()
 	var _data = bytes_to_var(serialized_behavior)
-	# TODO: Fix and uncomment when we network again.
-	#for serialized_rule in data:
-		#var rule = Rule.make(
-			#SkillManager.make_target_selection_instance(serialized_rule.target),
-			#SkillManager.make_action_instance(serialized_rule.action)
-		#)
-		#behavior.rules.append(rule)
+	# TODO: Implement when we network again.
 	return behavior
 
 func _to_string() -> String:
