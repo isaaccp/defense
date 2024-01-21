e73e48c4c432057e8fb4d5b23af62af6cd3f5ecd
Isaac Clerencia <isaac.clerencia@gmail.com>
2024-01-21 16:43:26 -0500

    Fix bit broken by target selection changes.
    Commit Details
    
    * Cycles before: 7, after: 7

--- behavior/actions/move_away_action.gd
+++ behavior/actions/move_away_action.gd
@@ -12,9 +12,8 @@ func _init():
 func post_initialize():
 	navigation_agent.velocity_computed.connect(_on_velocity_computed)
 	# Single-shot activation. There's no 'arriving' at running away.
-	navigation_agent.target_position = _nav_dest(target)
+	navigation_agent.target_position = _nav_dest()
 
-
-func _nav_dest(target: Target) -> Vector2:
+func _nav_dest() -> Vector2:
 	var dir = target_position().direction_to(body.global_position)
 	return body.position + dir * move_away_distance
